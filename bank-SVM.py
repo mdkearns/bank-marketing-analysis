74d19e9aed97f4b113367acd45d19a108250f6b1
Matt D. Kearns <mattdkearns@gmail.com>
2018-06-28 01:05:12 -0400

    commit SVM classifier

---
+++ bank-SVM.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import numpy as np
+from sklearn import svm
+from sklearn.model_selection import KFold
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+
+import os
+
+def main():
+
+    # read data and extract X and y
+    df = pd.read_csv('./bank-data/clean.csv', sep=';')
+    values = df.values
+    X = values[:,:-1]
+    y = values[:,-1]
+
+    sum = 0
+    for val in y:
+        if val == 0:
+            sum += 1
+
+    print('Naive Approach:', sum/len(y), '\t(Predict that no one subscribes)')
+
+    # perform k-fold cross-validation
+    kf = KFold(n_splits=10)
+
+    # initialize a SVM classifier
+    clf = svm.SVC()
+
+    ave = 0
+
+    X, y = shuffle(X, y)
+
+    for train_index, test_index in kf.split(X, y):
+
+        # fit the SVM classifier
+        clf.fit(X[train_index], y[train_index])
+        ave += clf.score(X[test_index], y[test_index])
+
+    ave /= 10
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.50)
+
+    clf.fit(X_train, y_train)
+    print(clf.score(X_test, y_test))
+
+    print(ave)
+    
+
+if __name__ == '__main__':
+    main()
